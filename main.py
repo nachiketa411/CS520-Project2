1975dc163d668eb6908eb5ca46d8f33b8b3aa7c6
Rutvi <rvs88@scarletmail.rutgers.edu>
2022-10-26 19:41:32 -0400

    Created BiBFS and Predator File. Updated main file to check BiBFS.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # This is a sample Python script.
 from Graph import Graph
 from GraphVisualizer import visualize
-
-
+from BiBFS import BidirectionalSearch
 
 
 
@@ -10,6 +9,7 @@ from GraphVisualizer import visualize
 if __name__ == '__main__':
     graph_1 = Graph()
     visualize(graph_1)
-
+    temp=BidirectionalSearch(graph_1.graph)
+    out=temp.bidirectional_search(2,8)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
