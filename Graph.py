fc0281ced0ccc3215d0048fd902f6816d17e533a
Nachiketa Prasad <nachiketa.411@gmail.com>
2022-10-26 19:05:26 -0400

    Nachi - Update Commit - 1. Updated Graph class to handle situations when filtered list returned empty

--- Graph.py
+++ Graph.py
@@ -20,13 +20,16 @@ class Graph:
                 self.graph[i] = [i - 1 % no_of_nodes, i + 1 % no_of_nodes]
         # we need to add the remaining edges according to the conditions given for the environment
         set_of_nodes_with_degree_2 = set(self.graph.keys())
+
+        # print statements
         print('degree 2: ', set_of_nodes_with_degree_2)
+
         while len(set_of_nodes_with_degree_2) > 0:
             random_node_choice = random.choice(list(set_of_nodes_with_degree_2))
             list_of_possible_connections = []
-            for i in range(2, 5):
-                list_of_possible_connections.append(random_node_choice - i % no_of_nodes)
-                list_of_possible_connections.append(random_node_choice + i % no_of_nodes)
+            for i in range(2, 6):
+                list_of_possible_connections.append((random_node_choice - i) % no_of_nodes)
+                list_of_possible_connections.append((random_node_choice + i) % no_of_nodes)
 
             print('list_of_possible_connections: ', list_of_possible_connections)
             # filter out invalid points i.e. points which are not in the set anymore
@@ -36,15 +39,26 @@ class Graph:
             print('filtered_list: ', filtered_list)
 
             if len(filtered_list) == 0:
+
                 # print statements
                 print(self.graph)
+                print(set_of_nodes_with_degree_2)
 
-                return self.graph
-            random_node_connection_choice = random.choice(filtered_list)
-            self.graph[random_node_choice].append(random_node_connection_choice)
-            self.graph[random_node_connection_choice].append(random_node_choice)
-            set_of_nodes_with_degree_2.remove(random_node_choice)
-            set_of_nodes_with_degree_2.remove(random_node_connection_choice)
+                set_of_nodes_with_degree_2.remove(random_node_choice)
+
+                # print statements
+                print(set_of_nodes_with_degree_2)
+            else:
+                random_node_connection_choice = random.choice(filtered_list)
+                self.graph[random_node_choice].append(random_node_connection_choice)
+                self.graph[random_node_connection_choice].append(random_node_choice)
+                set_of_nodes_with_degree_2.remove(random_node_choice)
+                set_of_nodes_with_degree_2.remove(random_node_connection_choice)
 
             # print statements
             print('Updated degree 2: ', set_of_nodes_with_degree_2)
+
+        # print statements
+        print('Final Graph: ', self.graph)
+
+        return self.graph
